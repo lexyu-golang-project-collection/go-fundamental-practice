7ae7936b013a5c03f3fcba933bcb4a2b822f89a7
LexYu <84486286+TLexYuW@users.noreply.github.com>
2023-11-13 23:21:27 +0800

    update closing channel

---
+++ go-kodekloud-course/closing-a-channel/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	ch := make(chan int, 10)
+
+	ch <- 10
+	ch <- 11
+
+	val, ok := <-ch
+	fmt.Println(val, ok)
+
+	close(ch)
+
+	val, ok = <-ch
+	fmt.Println(val, ok)
+
+	val, ok = <-ch
+	fmt.Println(val, ok)
+}
