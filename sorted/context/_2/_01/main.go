1d4d8261693fe1d14f7a5163551bb1ce1ac4016f
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-08-21 22:44:49 +0800

    update

--- sorted/context/_2/_01/main.go
+++ sorted/context/_2/_01/main.go
@@ -8,14 +8,14 @@ import (
 
 func Monitor(ctx context.Context) {
 	for {
-		fmt.Print("monitor")
+		fmt.Println("monitor...")
 	}
 }
 
 func main() {
-	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 	go Monitor(ctx)
 
-	time.Sleep(20 * time.Second)
+	time.Sleep(10 * time.Second)
 }
