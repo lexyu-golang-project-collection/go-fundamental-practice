b4aea17eb9627a43ec58827081739568bd459060
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-09-30 09:30:51 +0000

    update

---
+++ 100-go-mistakes/001_variable_shadowing/revise/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func test() (x int64, y int64) {
+	x = 3
+	y = 6
+
+	return
+}
+func main() {
+	var x int64 = 2
+	fmt.Printf("x %v \n", x)
+
+	if x, y := test(); x > 0 {
+		fmt.Printf("x %v y %v \n", x, y)
+	}
+
+	fmt.Printf("x %v \n", x)
+}
