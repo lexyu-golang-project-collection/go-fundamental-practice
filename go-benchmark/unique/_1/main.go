923e7392acac361ff0b9eee9c214a89c38e362b8
LexYu <84486286+TLexYuW@users.noreply.github.com>
2024-12-05 09:01:08 +0000

    update

--- go-benchmark/unique/_1/main.go
+++ go-benchmark/unique/_1/main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"testing"
+	"unique"
+)
 
+var (
+	s1 = "just a really long string that's really long"
+	s2 = "just a really long string that's really long"
+)
+
+func BenchmarkStringComparison(b *testing.B) {
+	// for i := 0; i < b.N; i++ {
+	// 	_ = (s1 == s2)
+	// }
+
+	for range b.N {
+		_ = (s1 == s2)
+	}
+}
+
+func BenchmarkUniqueStringComparison(b *testing.B) {
+	h1 := unique.Handle(s1)
+	h2 := unique.Handle(s2)
+	for range b.N {
+		_ = (h1 == h2)
+	}
 }
