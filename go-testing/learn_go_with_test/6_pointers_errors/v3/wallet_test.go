1b7ac6c6db6979b82888aa2549ee3636a9f7915d
LexYu <84486286+TLexYuW@users.noreply.github.com>
2025-02-16 23:53:16 +0800

    update

---
+++ go-testing/learn_go_with_test/6_pointers_errors/v3/wallet_test.go
@@ -0,0 +1,33 @@
+package pointers_errors
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+
+		wallet.Deposit(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s wnat %s", got, want)
+		}
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+		wallet := Wallet{balance: Bitcoin(20)}
+
+		wallet.Withdraw(Bitcoin(10))
+
+		got := wallet.Balance()
+
+		want := Bitcoin(10)
+
+		if got != want {
+			t.Errorf("got %s wnat %s", got, want)
+		}
+	})
+}
