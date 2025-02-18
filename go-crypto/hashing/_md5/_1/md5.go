ebcb6fb714c35c7343d6601b27915c3f3d3a1c0d
LexYu <84486286+TLexYuW@users.noreply.github.com>
2025-02-18 23:45:05 +0800

    Update md5.go

--- go-crypto/hashing/_md5/_1/md5.go
+++ go-crypto/hashing/_md5/_1/md5.go
@@ -1,6 +1,23 @@
 package main
 
+import (
+	"crypto/md5"
+	"fmt"
+)
+
+func MD5_1(str string) string {
+	hash := md5.New()
+	_, err := hash.Write([]byte(str))
+	if err != nil {
+		panic(err)
+	}
+	sum := hash.Sum(nil)
+
+	return fmt.Sprintf("%x\n", sum)
+}
+
 func main() {
 	str := "Gopher"
-
+	res := MD5_1(str)
+	fmt.Println(res)
 }
